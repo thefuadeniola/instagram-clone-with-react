836c5e9034b5727a3111adee956451100c07d5b2
Olaleye Hairat <SammieKei40>
2022-09-23 07:18:31 +0100

    Fixed Link

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -57,7 +57,7 @@ export default function Signup() {
                 </Card.Body>
             </Card>
             <div className='w-100 text-center mt-2'>
-                Already have an account? <Link to='/login'>Login</Link>
+                Already have an account? <Link to='/'>Login</Link>
             </div>
         </div>
     )
